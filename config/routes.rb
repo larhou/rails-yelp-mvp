2ebb6f7c4d217b1f0e323f5d9be22f691cd6f737
Lars Houbak <lars.houbak@gmail.com>
2019-11-06 18:11:03 +0100

    task 2 - day 28

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,16 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+  root to: 'restaurants#index'
+
+  resources :restaurants, only: [:index, :new, :create, :show] do
+    resources :reviews, only: [:new, :create]
+  end
+
 end
+
+ # get 'review/create'
+ # get 'review/new'
+ # get 'restaurant/index'
+ # get 'restaurant/create'
+ # get 'restaurant/new'
+ # get 'restaurant/show'
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,28 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+Restaurant.destroy_all
+
+require 'faker'
+puts 'Creating 10 restaurants...'
+
+10.times do
+  category = ['chinese', 'italian', 'japanese', 'french', 'belgian']
+  restaurant = Restaurant.create(
+    name: Faker::Restaurant.name,
+    address: Faker::Address.full_address,
+    phone_number: Faker::PhoneNumber.phone_number_with_country_code,
+    category: category.sample
+  )
+
+  5.times do
+    rating = [0, 1, 2, 3, 4, 5]
+    content = Faker::Restaurant.review
+    review = Review.new(
+      content: content,
+      rating: rating.sample
+    )
+    review.restaurant = restaurant
+    review.save
+  end
+end
+
+puts 'Finished!'
+
